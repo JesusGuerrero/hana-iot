8d062bdbfc662c41e9404dade6fd302e81c8d83f
Hana Huayu Heh <anubiss13477@gmail.com>
2017-05-16 21:27:42 -0700

    server

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ var ledState = 0;
 
 
 // Twilio, the SMS system servicegit
-// var accountSid = 'AC70731db98f0a7ad0863697704e8e4716';
-// var authToken = '281c19c81e4762364b53524f5bf7eadc';
-// var twilio = require('twilio');
-// var client = new twilio(accountSid, authToken);
+   var accountSid = 'AC70731db98f0a7ad0863697704e8e4716';
+   var authToken = '281c19c81e4762364b53524f5bf7eadc';
+   var twilio = require('twilio');
+   var client = new twilio(accountSid, authToken);
 
 
 //ip address
@@ -45,9 +45,13 @@ require('mahrio').runServer( process.env, __dirname ).then( function( server ) {
       //LED
       socket.on('event:light', function () {
           console.log("turn on light ");
-          ledState = !ledState;
-          led1.writeSync(ledState);
-          led2.writeSync(ledState);
+          ledState = ledState + 1;
+
+console.log(ledState);
+          led1.writeSync(ledState % 2);
+          led2.writeSync(ledState % 2);
+	  led3.writeSync(ledState % 2);
+          led4.writeSync(ledState % 2);
       });
       //Buzzer
       socket.on('event:buzzer', function () {
@@ -57,26 +61,28 @@ require('mahrio').runServer( process.env, __dirname ).then( function( server ) {
               buzzer.writeSync(0);
           }, 3000);
       });
-      // socket.on('event:textSMS', function () {
-      //     motion.watch( function(err, val) {
-      //         if (err) {
-      //             console.log('Motion in 21 Error');
-      //             return;
-      //         }
-      //         if (val) {
-      //             console.log('motion sensor detect something');
-      //             // client.messages.create({
-      //             //     body: 'Peeper Feeder detects some motions, check it out on Peeper App ',
-      //             //     to: '+14159990504',  // Text this number
-      //             //     from: '+14159694541' // From a valid Twilio number
-      //             // }).then(function(message){
-      //             //     console.log(message.sid)
-      //             //     console.log('message sent');
-      //             // });
-      //         }
-      //     });
-      //
-      // });
+         socket.on('event:textSMS', function () {
+	     console.log("turn on SMS ");
+             
+	     motion.watch( function(err, val) {
+                 if (err) {
+                     console.log('Motion in 21 Error');
+                     return;
+                 }
+                 if (val) {
+                     console.log('motion sensor detect something');
+                     // client.messages.create({
+                     //     body: 'Peeper Feeder detects some motions, check it out on Peeper App ',
+                     //     to: '+14159990504',  // Text this number
+                     //     from: '+14159694541' // From a valid Twilio number
+                     // }).then(function(message){
+                     //     console.log(message.sid)
+                     //     console.log('message sent');
+                     // });
+                 }
+             });
+        
+         });
 
 
 
